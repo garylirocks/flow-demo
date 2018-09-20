45b46caee6fd573fac27b9bbe3ed1564071bc2dc
garylirocks <gary.li.rocks@gmail.com>
2018-09-20 18:43:15 +1200

    add a HOC example

--- src/hello.js
+++ src/hello.js
@@ -1,9 +1,39 @@
 /* @flow */
 
-function foo(name: string): string {
-    return `Hello ${name}`;
-    // return 9;
-    // return null;
+import * as React from 'react';
+
+function mapProps<PropsInput: {}, PropsOutput: {}>(
+  mapperFn: (PropsInput) => PropsOutput,
+): (React.ComponentType<PropsOutput>) => React.ComponentType<PropsInput> {
+  return Component => {
+
+    // new Component
+    return props => {
+        const newProps = mapperFn(props);
+        <Component {...newProps} />
+    }
+  };
 }
 
-foo('gary');
+type Props = {
+    x: number,
+    y: number,
+};
+
+class myComponent extends React.Component<Props> {
+    render() {
+        return (
+        <div>
+            <span>{this.props.x}</span>
+            <span>{this.props.y}</span>
+        </div>
+        );
+    }
+}
+
+
+const WrappedComponent = mapProps((o) => ({...o, y: 30}))(myComponent);
+
+<WrappedComponent x={'haha'} />
+
+
